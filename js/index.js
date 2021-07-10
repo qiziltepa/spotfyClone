be882e97fd38286e83dec2bf166fed651d4715d2
salohiddin1221 <salohiddinkhayitov0@gmail.com>
2021-07-10 19:25:59 +0500

    sign up end

--- js/index.js
+++ js/index.js
@@ -1,7 +1,27 @@
 const barsIcon = document.querySelector('.bars__icons');
-const navbar = document.querySelector('.navbar__links')
+const navbar = document.querySelector('.navbar__links');
+const close = document.querySelector('.fa-times');
+const form = document.querySelectorAll('.sign');
+const signUp = document.querySelector('.button_up');
+
+
+
+// !mobile navbar codes start
 
 barsIcon.addEventListener('click', () => {
     barsIcon.classList.toggle('active');
     navbar.classList.toggle('active')
+})
+
+// !mobile navbar codes end
+signUp.addEventListener('click', () => {
+    form.forEach(item => {
+        item.classList.add('active')
+    })
+})
+
+close.addEventListener('click', () => {
+    form.forEach(item => {
+        item.classList.remove('active')
+    })
 })
