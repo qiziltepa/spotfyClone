c36d683b59111d5447ae78bd0a95c8d1261126b1
Developer0772 <alishernarzullayev7777@gmail.com>
2021-07-13 10:40:15 +0500

    push addMedia

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
+var id=0,
+cols="col-"+id,
+part=document.createElement('div'),
+divInfo=document.createElement('div'),
+col=document.createElement('div');
+col.id=cols;
+col.className='col'
+
+
+h2=document.createElement('h2'),
+h2.innerText="Favorite"
+h2.className='information';
+h2.id='info'
+
+
+part.appendChild(divInfo)
+divInfo.className='info';
+divInfo.innerHTML=`<p class="text" id="prev" onclick="plusDivs(-1,'col0')"><i class="material-icons">navigate_before</i></p><p class="text" id="next" onclick="plusDivs(1,'col0')"><i class="material-icons">navigate_next</i></p>`;
+divInfo.insertAdjacentElement('afterbegin',h2)
+part.className='part part-'+id;
+part.id='part-'+id;
+part.appendChild(col)
+console.log(part);
+
 function MusicItem(musicname,artist,seen,musicNamIid,artistId,seenId,src,href){
     
 // variable create
@@ -8,14 +32,9 @@ pSeen=document.createElement('p'),
 div=document.createElement('div'),
 img=document.createElement('img'),
 i=document.createElement('i'),
-col=document.querySelector("#col-1");
-
-
-var divImg=document.createElement('div'),
+divImg=document.createElement('div'),
 divPlay=document.createElement('div'),
 divText=document.createElement('div');
-
-
 divImg.className='item-img'
 divText.className='item-text'
 divPlay.className='item-play'
@@ -45,8 +64,14 @@ div.appendChild(divImg);
 div.appendChild(divText);
 col.appendChild(a);
 }
-//musicLength bu - musiclar uzunligi bo'yichadir
-var musicLength=15;
-for(var i=0;i<musicLength;i++){
-    MusicItem("Rap God","Eminem",1000,"musicName-1","artist-1","seen-1","./img/back-image.jpg","https://open.spotify.com")
-}
+var m=document.getElementById('main');
+m.appendChild(part);
+// its is example for test
+
+MusicItem("The Real Slim Shady","Eminem",1200,"musicName-1","artist-1","seen-1","./img/back-image.jpg","https://open.spotify.com")
+MusicItem("Without me","EminemEminemm",1000,"musicName-1","artist-1","seen-1","./img/back-image.jpg","https://open.spotify.com")
+MusicItem("Rap God","Eminem",1100,"musicName-1","artist-1","seen-1","./img/back-image.jpg","https://open.spotify.com")
+
+// module exports from here to server
+
+module.exports.MusicItem=MusicItem;
--- server.js
+++ server.js
@@ -1,1 +1,2 @@
-
+var addMedia=require("./js/index")
+// addMedia.MusicItem(here write about music name, artist, id and each.)
